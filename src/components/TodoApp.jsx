31f2722de51e239092d8c01fa0069878c48503a9
Salomon Tshobohwa <stshobohwa@gmail.com>
2023-04-05 13:22:37 +0200

    Create first component

---
+++ src/components/TodoApp.jsx
@@ -0,0 +1,9 @@
+const TodoApp = () => {
+  return (
+    <React.Fragment>
+      <h1>Hello world!</h1>
+      <p>I am in a React Component!</p>
+    </React.Fragment>
+  );
+};
+export default TodoApp;
